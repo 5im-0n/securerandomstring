20def6c1897d7b0837be13d20c13c3f5de6823a8
s2 <s2@31337.it>
2015-04-22 20:47:04 +0200

    initial commit

---
+++ tests.js
@@ -0,0 +1,53 @@
+var securerandomstring = require('./lib/securerandomstring.js');
+
+
+//my awesome test framework
+var test = function(name, what, ref, c) {
+	process.stdout.write(name);
+
+	if (typeof(c) !== 'boolean') {
+		c = true;
+	}
+
+	if (typeof(what) === 'function') {
+		what = what();
+	}
+	if (typeof(ref) === 'function') {
+		ref = ref();
+	}
+
+	if ((what === ref) === c) {
+		process.stdout.write(' ✓');
+	} else {
+		process.stdout.write(' ⛝\n');
+		process.stdout.write('    ----> expected | ' + (typeof(ref) === 'string' ? ref.replace(/ /g, '·') + '\n' : ref + '\n'));
+		process.stdout.write('    ----> got      | ' + (typeof(what) === 'string' ? what.replace(/ /g, '·') + '\n' : what + '\n'));
+	}
+	process.stdout.write('\n');
+};
+
+
+
+//the actual tests
+securerandomstring(function(sr) {
+	test('generate a random string 32 chars long',
+		sr.length,
+		32
+	);
+});
+
+securerandomstring(function(sr) {
+	test('generate a random string 1 char long',
+		sr.length,
+		1
+	);
+}, {length: 1});
+
+securerandomstring(function(sr) {
+	test('generate a random string 256 chars long',
+		sr.length,
+		256
+	);
+}, {length: 256});
+
+
